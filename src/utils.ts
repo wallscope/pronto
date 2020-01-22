77dc4cfc50d5eddaad80322649aa879ea1698d5b
belvederef <francescobelvedere@hotmail.com>
2020-01-22 11:09:31 +0000

    Add prefixes list in var

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,23 @@
+export const prefixes = {
+  rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+  rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+  owl: 'http://www.w3.org/2002/07/owl#',
+  a: 'http://proton.semanticweb.org/protonsys#',
+  dct: 'http://purl.org/dc/terms/',
+  skos: 'http://www.w3.org/2004/02/skos/core#',
+  madsrdf: 'http://www.loc.gov/mads/rdf/v1#',
+  dc: 'http://purl.org/dc/elements/1.1/',
+  vs: 'http://www.w3.org/2003/06/sw-vocab-status/ns#',
+  foaf: 'http://xmlns.com/foaf/0.1/',
+  schema: 'http://schema.org/',
+  wdrs: 'http://www.w3.org/2007/05/powder-s#',
+  cc: 'http://creativecommons.org/ns#',
+  vann: 'http://purl.org/vocab/vann/',
+  ov: 'http://open.vocab.org/terms/',
+  grddl: 'http://www.w3.org/2003/g/data-view#',
+  prov: 'http://www.w3.org/ns/prov#',
+};
+
 export function copyToClipboard(text: string) {
   if (window.clipboardData && window.clipboardData.setData) {
     // IE specific code path to prevent textarea being shown while dialog is visible.
--- src/views/About.vue
+++ src/views/About.vue
@@ -12,24 +12,18 @@
         .eight.wide.tablet.six.wide.computer.sixteen.wide.mobile.centered.left.aligned.column
           h2 List of the included ontologies
           .ui.list
-            a.item http://www.w3.org/1999/02/22-rdf-syntax-ns#
-            a.item http://www.w3.org/2000/01/rdf-schema#
-            a.item http://www.w3.org/2002/07/owl#
-            a.item http://proton.semanticweb.org/protonsys#
-            a.item http://purl.org/dc/terms/
-            a.item http://www.w3.org/2004/02/skos/core#
-            a.item http://www.loc.gov/mads/rdf/v1#
-            a.item http://purl.org/dc/elements/1.1/
-            a.item http://www.w3.org/2003/06/sw-vocab-status/ns#
-            a.item http://xmlns.com/foaf/0.1/
-            a.item http://schema.org/
-            a.item http://www.w3.org/2007/05/powder-s#
-            a.item http://creativecommons.org/ns#
-            a.item http://purl.org/vocab/vann/
-            a.item http://open.vocab.org/terms/
-            a.item http://www.w3.org/2003/g/data-view#
-            a.item http://www.w3.org/ns/prov#
+            a.item(v-for="p in Object.values(prefixes)") 
+              | {{ p }}
 </template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { prefixes } from '@/utils';
+
+@Component
+export default class About extends Vue {
+  prefixes = prefixes;
+}
+</script>
 
 <style lang="scss" scoped>
 #about-text {
