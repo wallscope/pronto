60db55fb763a98fee7154007bac0add5b39b0594
belvederef <francescobelvedere@hotmail.com>
2020-02-11 14:42:32 +0000

    Add feedback form Move components related to each other under same folder

---
+++ frontend/src/views/main/Feedback.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  .row(v-if="isFeedbackOpen && showFeedback()")
+    .sixteen.wide.tablet.sixteen.wide.computer.sixteen.wide.mobile.centered.column
+      .ui.info.message
+        i.close.icon(@click="closeFeedback()")
+        .header
+          | Do you like Pronto?
+
+        .ui.form.feedback
+          .field
+            label Give us some feedback so we can improve it!
+            textarea(
+              rows='2', 
+              placeholder='Type your feedback...'
+            )
+          button.ui.button Send
+
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'vue-property-decorator';
+import TextHighlight from 'vue-text-highlight';
+import { OntologyResult } from '@/types';
+
+@Component
+export default class Feedback extends Vue {
+  @Prop({ default: false }) isFeedbackOpen!: boolean;
+
+  showFeedback() {
+    const lastDismissed = localStorage.getItem('feedbackLastDismissed');
+    if (!lastDismissed) return true;
+
+    // If dismissed more than three days ago return true
+    const timeDifference = +new Date() - +new Date(lastDismissed);
+    if (timeDifference > 3 * 24 * 3600) return true;
+
+    this.$emit('close-feedback');
+    return false;
+  }
+  closeFeedback() {
+    localStorage.setItem('feedbackLastDismissed', new Date().toString());
+    // localStorage.setItem('wasFeedbackDismissed', 'true');
+    this.$emit('close-feedback');
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
